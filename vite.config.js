b9ef7c723b54823fb206806efff7c7610876146f
jbrasay <epiksushimisc@gmail.com>
2024-10-31 19:22:13 -1000

    Update vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactApp-gh-actions/"
+  base: "/reactApp-gh-actions/",
 })
